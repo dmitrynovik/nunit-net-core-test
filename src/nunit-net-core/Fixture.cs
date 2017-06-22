f504db9e4f5b40c334696fa64b5172edf06853d7
dmitrynovik <cryinstone@gmail.com>
2017-06-23 08:16:01 +1000

    Init

---
+++ src/nunit-net-core/Fixture.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace nunit_net_core
+{
+    [TestFixture]
+    public class Fixture
+    {
+        [Test]
+        public void Foo()
+        {
+        }
+    }
+}
